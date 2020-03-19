ae191d04d78cb22b439b6d438f8d563a6f908070
myname21-meet <myname21@meet.mit.edu>
2020-03-19 19:21:32 +0200

    first committt

---
+++ app.py
@@ -0,0 +1,21 @@
+#from database import *
+from flask import Flask, render_template, url_for, request, redirect
+from flask import session as login_session
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'greenwallx4@gmail.com'
+app.config['MAIL_PASSWORD'] = 'funwithmeet19'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
